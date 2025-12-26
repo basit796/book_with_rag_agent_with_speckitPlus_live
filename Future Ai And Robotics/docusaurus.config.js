5323b11bc1d9e59f86e8566b9b39c04971384b4d
basit796 <basitkhalid796@gmail.com>
2025-12-26 18:13:39 +0500

    Configure GitHub Pages deployment for Docusaurus site

--- Future Ai And Robotics/docusaurus.config.js
+++ Future Ai And Robotics/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://basit796.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/book_with_rag_agent_with_speckitPlus_live/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'basit796', // Usually your GitHub org/user name.
+  projectName: 'book_with_rag_agent_with_speckitPlus_live', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
